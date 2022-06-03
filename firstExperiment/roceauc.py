13b0e8f156422a311b93135ea890e590ecfe0946
BrattaFrancesco <fra.bra00@gmail.com>
2022-06-03 09:39:53 +0200

    Feature secondExperiment

--- firstExperiment/roceauc.py
+++ firstExperiment/roceauc.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     models = []
     models.append(RandomForestClassifier(max_depth=100, n_estimators=500))
-    models.append(SVC(gamma=0.001, probability=True))
+    models.append(SVC(gamma=0.001, probability=True, kernel="poly"))
     models.append(KNeighborsClassifier(n_neighbors=3))
     models.append(DecisionTreeClassifier(max_depth=100))
 
@@ -28,7 +28,9 @@ if __name__ == '__main__':
         print(model)
         for file in os.listdir(featuresPath):
             plt.figure(file)
+            print(file.split(".")[0])
             features = pd.read_csv(featuresPath + file, delimiter=";", skipinitialspace=True)
+            # Create the normalizer and read the input
             mm = make_pipeline(MinMaxScaler(), Normalizer())
             X = mm.fit_transform(features.iloc[:, 2:])
             y = LabelEncoder().fit_transform(features['UT'])
@@ -37,27 +39,29 @@ if __name__ == '__main__':
             # Binarize the output
             y_bin = label_binarize(y, classes=[0, 1, 2, 3, 4, 5])
             n_classes = y_bin.shape[1]
-
+            print("N classes: ", n_classes)
+            # Fit the model with a 10-fold cross validation method
             pipe = Pipeline([('scaler', MinMaxScaler()), ('clf', model)])
-            # or
-            #clf = OneVsRestClassifier(LogisticRegression())
-            #pipe= Pipeline([('scaler', StandardScaler()), ('clf', clf)])
             y_score = cross_val_predict(pipe, X, y, cv=10, method='predict_proba')
             fpr = dict()
             tpr = dict()
             fnr = dict()
             roc_auc = dict()
+
+            # Compute the roc curve for each class
             for i in range(n_classes):
                 fpr[i], tpr[i], _ = roc_curve(y_bin[:, i], y_score[:, i])
                 fnr[i] = 1 - tpr[i]
                 roc_auc[i] = auc(fpr[i], tpr[i])
             colors = cycle(['blue', 'red', 'green', 'yellow', 'purple', 'black'])
             print(file.split(".")[0])
+
+            # Print the roc curve
             for i, color in zip(range(n_classes), colors):
                 plt.plot(fpr[i], tpr[i], color=color, lw=lw,
                          label='ROC curve of class {0} (AUC = {1:0.2f}) (EER = {2:0.2f}%)'
-                         ''.format(i, roc_auc[i], fpr[i][np.nanargmin(np.absolute((fnr[i] - fpr[i])))]*100))
-                print("EER class ", i, ": ", fpr[i][np.nanargmin(np.absolute((fnr[i] - fpr[i])))]*100, "%")
+                               ''.format(i, roc_auc[i], fpr[i][np.nanargmin(np.absolute((fnr[i] - fpr[i])))] * 100))
+                # print("EER class ", i, ": ", fpr[i][np.nanargmin(np.absolute((fnr[i] - fpr[i])))]*100, "%")
             print()
             plt.plot([0, 1], [0, 1], 'k--', lw=lw)
             plt.xlim([-0.05, 1.0])
